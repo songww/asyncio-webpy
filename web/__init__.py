9f220301d68f6af48fbab1e9524884933d58b738
songww <sww4718168@gmail.com>
2018-10-12 13:51:50 +0800

    change version to 0.01-dev1

--- web/__init__.py
+++ web/__init__.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 """web.py: makes web apps (http://webpy.org)"""
 
-from __future__ import generators
-
-__version__ = "0.40-dev1"
+__version__ = "0.01-dev1"
 __author__ = [
     "Aaron Swartz <me@aaronsw.com>",
     "Anand Chitipothu <anandology@gmail.com>"
@@ -30,4 +28,3 @@ try:
     from . import webopenid as openid
 except ImportError:
     pass # requires openid module
-
